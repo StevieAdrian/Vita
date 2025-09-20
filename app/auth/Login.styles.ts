273370a2a433cbbbf1ff2317e74c92480692b7a8
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2025-09-21 01:38:05 +0700

    feat: add login fe

--- app/auth/Login.styles.ts
+++ app/auth/Login.styles.ts
@@ -1,13 +1,101 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
+    imageContainer: {
         flex: 1,
         backgroundColor: "#1A73E8",
     },
+    container: {
+        flex: 1,
+        backgroundColor: "#1A73E8",
+        alignItems: "center",
+    },
     welcomeImage: {
         width: "70%", 
-        marginLeft: 30,
-        resizeMode: "contain" as const,
-    }
+        resizeMode: "contain",
+        alignSelf: "flex-start",    
+        marginLeft: 30,          
+        marginBottom: -50,
+    },
+    boxContainer: {
+        width: "85%",
+        backgroundColor: "white",
+        borderRadius: 12,
+        padding: 20,
+        alignItems: "center",
+    },
+    innerContainer: {
+        width: "100%",
+        borderWidth: 1,
+        borderColor: "#ccc",
+        borderRadius: 8,
+        marginBottom: 15,
+        paddingHorizontal: 10,
+    },
+    label: {
+        alignSelf: "flex-start",  
+        fontSize: 14,
+        color: "#333",
+        marginBottom: 5,
+        fontWeight: 500,
+    },
+    required: {
+        color: "red",
+    },
+    input: {
+        height: 45,
+        fontSize: 14,
+        color: "#333",
+    },
+    continueButton: {
+        backgroundColor: "black",
+        width: "100%",
+        paddingVertical: 12,
+        borderRadius: 8,
+        alignItems: "center",
+        marginTop: 10,
+    },
+    continueText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: 400,
+    },
+    signupText: {
+        fontSize: 13,
+        color: "black",
+        marginTop: 15,
+    },
+    signupLink: {
+        color: "blue",
+        fontWeight: "bold",
+    },
+    dividerWrapper: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginVertical: 20,
+        width: "70%",       
+        alignSelf: "center",
+    },
+    divider: {
+        flex: 1,
+        height: 1.5,
+        backgroundColor: "white",
+    },
+    dividerText: {
+        marginHorizontal: 10,
+        color: "white",
+        fontSize: 14,
+    },
+    googleButton: {
+        width: "70%",
+        height: 50,
+        backgroundColor: "transparent",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    googleLogo: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain",
+    },
 });
