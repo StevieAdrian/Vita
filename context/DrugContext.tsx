5bb94f059810927deaff12b38e855915e67c2295
Bryan Cornelius Sunadi <bryansunadi@gmail.com>
2025-10-02 14:20:10 +0700

    feat: Fixed CRUD for Drugs Card

---
+++ context/DrugContext.tsx
@@ -0,0 +1,81 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { DrugReminder } from "../constants/drugs";
+import { useAuthState } from "../hooks/useAuthState";
+import {
+  createDrug,
+  deleteDrugs,
+  getDrugByUser,
+  updateDrugs,
+} from "../services/drug.service";
+
+interface DrugContextType {
+  drugs: DrugReminder[];
+  loading: boolean;
+  add: (data: DrugReminder) => Promise<void>;
+  update: (id: string, data: Partial<DrugReminder>) => Promise<void>;
+  remove: (id: string) => Promise<void>;
+}
+
+const DrugContext = createContext<DrugContextType | undefined>(undefined);
+
+export const DrugProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const { user } = useAuthState();
+  const [drugs, setDrugs] = useState<DrugReminder[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!user) {
+      setLoading(false);
+      return;
+    }
+
+    const loadData = async () => {
+      try {
+        const data = await getDrugByUser(user.uid);
+        setDrugs(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadData();
+  }, [user]);
+
+  const remove = async (id: string) => {
+    await deleteDrugs(id);
+    setDrugs((prev) => prev.filter((drug) => drug.id !== id));
+  };
+
+  const add = async (data: DrugReminder) => {
+    const { id, ...dataWithoutId } = data;
+    const newId = await createDrug({
+      ...dataWithoutId,
+      userId: user?.uid ?? "",
+    } as any);
+    setDrugs((prev) => [...prev, { ...dataWithoutId, id: newId }]);
+  };
+
+  const update = async (id: string, data: Partial<DrugReminder>) => {
+    await updateDrugs(id, data);
+    setDrugs((prev) =>
+      prev.map((item) => (item.id === id ? { ...item, ...data } : item))
+    );
+  };
+
+  return (
+    <DrugContext.Provider value={{ drugs, loading, add, update, remove }}>
+      {children}
+    </DrugContext.Provider>
+  );
+};
+
+export const useDrugs = () => {
+  const context = useContext(DrugContext);
+  if (!context) {
+    throw new Error(context);
+  }
+  return context;
+};
